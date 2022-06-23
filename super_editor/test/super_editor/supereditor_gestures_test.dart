8940128e3b973fb870bd9fefeafdbb04c062ac6c
Matt Carroll <matthew-carroll@users.noreply.github.com>
2022-06-23 16:27:25 -0700

    WIP: Fixed bug when selecting beyond component bounds, and added tests

--- super_editor/test/super_editor/supereditor_gestures_test.dart
+++ super_editor/test/super_editor/supereditor_gestures_test.dart
@@ -1,11 +1,16 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:logging/logging.dart';
+import 'package:super_editor/super_editor.dart';
 
 import '../test_tools.dart';
 import 'document_test_tools.dart';
+import 'supereditor_inspector.dart';
+import 'supereditor_robot.dart';
 
 void main() {
   group("SuperEditor gestures", () {
-    testWidgetsOnDesktop("dragging a selection above a component selects to the beginning of the component",
+    testWidgetsOnDesktop(
+        "dragging a single component selection above a component selects to the beginning of the component",
         (tester) async {
       // For example, a user drags to select text in a paragraph. The user
       // is dragging the cursor up the center of the paragraph. When the cursor
@@ -13,22 +18,186 @@ void main() {
       // beginning of the paragraph, rather than get stuck in the middle of the
       // top line of text.
 
-//       await tester.createDocument()
-//           .fromMarkdown('''
-// This is a paragraph of text that
-// spans multiple lines.''')
+      await tester
+          .createDocument()
+          .fromMarkdown(
+            '''
+This is a paragraph of text that
+spans multiple lines.''',
+          )
+          .forDesktop()
+          .pump();
 
-      // TODO:
+      final document = SuperEditorInspector.findDocument()!;
+      final paragraphNode = document.nodes.first as ParagraphNode;
+
+      await tester.dragSelectFromPositionByOffset(
+        from: DocumentPosition(
+          nodeId: paragraphNode.id,
+          nodePosition: paragraphNode.endPosition,
+        ),
+        delta: const Offset(0, -300),
+      );
+
+      // Ensure that the entire paragraph is selected, after dragging
+      // above it.
+      expect(
+        SuperEditorInspector.findDocumentSelection(),
+        DocumentSelection(
+          base: DocumentPosition(
+            nodeId: paragraphNode.id,
+            nodePosition: paragraphNode.endPosition,
+          ),
+          extent: DocumentPosition(
+            nodeId: paragraphNode.id,
+            nodePosition: paragraphNode.beginningPosition,
+          ),
+        ),
+      );
     });
 
-    testWidgetsOnDesktop("dragging a selection below a component selects to the end of the component", (tester) async {
+    testWidgetsOnDesktop("dragging a single component selection below a component selects to the end of the component",
+        (tester) async {
       // For example, a user drags to select text in a paragraph. The user
       // is dragging the cursor down the center of the paragraph. When the cursor
       // moves below the paragraph, the selection extent should move to the
       // end of the paragraph, rather than get stuck in the middle of the
       // bottom line of text.
 
-      // TODO:
+      await tester
+          .createDocument()
+          .fromMarkdown(
+            '''
+This is a paragraph of text that
+spans multiple lines.''',
+          )
+          .forDesktop()
+          .pump();
+
+      final document = SuperEditorInspector.findDocument()!;
+      final paragraphNode = document.nodes.first as ParagraphNode;
+
+      await tester.dragSelectFromPositionByOffset(
+        from: DocumentPosition(
+          nodeId: paragraphNode.id,
+          nodePosition: paragraphNode.beginningPosition,
+        ),
+        delta: const Offset(0, 300),
+      );
+
+      // Ensure that the entire paragraph is selected, after dragging
+      // below it.
+      expect(
+        SuperEditorInspector.findDocumentSelection(),
+        DocumentSelection(
+          base: DocumentPosition(
+            nodeId: paragraphNode.id,
+            nodePosition: paragraphNode.beginningPosition,
+          ),
+          extent: DocumentPosition(
+            nodeId: paragraphNode.id,
+            nodePosition: paragraphNode.endPosition,
+          ),
+        ),
+      );
+    });
+
+    testWidgetsOnDesktop(
+        "dragging a multi-component selection above a component selects to the beginning of the top component",
+        (tester) async {
+      // For example, a user drags to select text in a paragraph. The user
+      // is dragging the cursor up the center of the paragraph. When the cursor
+      // moves above the paragraph, the selection extent should move to the
+      // beginning of the paragraph, rather than get stuck in the middle of the
+      // top line of text.
+
+      await tester
+          .createDocument()
+          .fromMarkdown(
+            '''
+# This is a test
+This is a paragraph of text that
+spans multiple lines.''',
+          )
+          .forDesktop()
+          .pump();
+
+      final document = SuperEditorInspector.findDocument()!;
+      final titleNode = document.nodes.first as ParagraphNode;
+      final paragraphNode = document.nodes[1] as ParagraphNode;
+
+      await tester.dragSelectFromPositionByOffset(
+        from: DocumentPosition(
+          nodeId: paragraphNode.id,
+          nodePosition: paragraphNode.endPosition,
+        ),
+        delta: const Offset(0, -300),
+      );
+
+      // Ensure that the entire paragraph is selected, after dragging
+      // above it.
+      expect(
+        SuperEditorInspector.findDocumentSelection(),
+        DocumentSelection(
+          base: DocumentPosition(
+            nodeId: paragraphNode.id,
+            nodePosition: paragraphNode.endPosition,
+          ),
+          extent: DocumentPosition(
+            nodeId: titleNode.id,
+            nodePosition: titleNode.beginningPosition,
+          ),
+        ),
+      );
+    });
+
+    testWidgetsOnDesktop(
+        "dragging a multi-component selection below a component selects to the end of the bottom component",
+        (tester) async {
+      // For example, a user drags to select text in a paragraph. The user
+      // is dragging the cursor up the center of the paragraph. When the cursor
+      // moves above the paragraph, the selection extent should move to the
+      // beginning of the paragraph, rather than get stuck in the middle of the
+      // top line of text.
+
+      await tester
+          .createDocument()
+          .fromMarkdown(
+            '''
+# This is a test
+This is a paragraph of text that
+spans multiple lines.''',
+          )
+          .forDesktop()
+          .pump();
+
+      final document = SuperEditorInspector.findDocument()!;
+      final titleNode = document.nodes.first as ParagraphNode;
+      final paragraphNode = document.nodes[1] as ParagraphNode;
+
+      await tester.dragSelectFromPositionByOffset(
+        from: DocumentPosition(
+          nodeId: titleNode.id,
+          nodePosition: titleNode.beginningPosition,
+        ),
+        delta: const Offset(0, 300),
+      );
+
+      // Ensure that the entire paragraph is selected, after dragging
+      // above it.
+      expect(
+        SuperEditorInspector.findDocumentSelection(),
+        DocumentSelection(
+          base: DocumentPosition(
+            nodeId: titleNode.id,
+            nodePosition: titleNode.beginningPosition,
+          ),
+          extent: DocumentPosition(
+            nodeId: paragraphNode.id,
+            nodePosition: paragraphNode.endPosition,
+          ),
+        ),
+      );
     });
   });
 }
